38d18c8de50b961dcf0aca8a1cb3274ec5af07bf
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-23 19:29:45 +0200

    Update coffee_bot_2c_saving.py

--- coffee_bot_2c_saving.py
+++ coffee_bot_2c_saving.py
@@ -24,6 +24,64 @@ from langchain_huggingface import HuggingFaceEndpoint, HuggingFaceEmbeddings
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings  # Import corrected
 
 
+#connection to huggingface
+huggingface_token = st.secrets["df_token"]
+login(token=huggingface_token)
+
+# This info is at the top of each HuggingFace model page
+hf_model = "mistralai/Mistral-7B-Instruct-v0.3"
+llm = HuggingFaceEndpoint(repo_id=hf_model)
+
+# Initialize HuggingFace embeddings
+embedding_model = "sentence-transformers/all-MiniLM-l6-v2"
+embeddings_folder = "coffee_content/embeddings"
+load_path = "coffee_content/faiss_index"
+os.makedirs(embeddings_folder, exist_ok=True)
+os.makedirs(load_path, exist_ok=True)
+
+embeddings = HuggingFaceEmbeddings(model_name=embedding_model, cache_folder=embeddings_folder)
+
+# Check if FAISS files exist before loading
+faiss_index_file = os.path.join(load_path, "index.faiss")
+faiss_pkl_file = os.path.join(load_path, "index.pkl")
+
+if os.path.exists(faiss_index_file) and os.path.exists(faiss_pkl_file):
+    vector_db = FAISS.load_local(load_path, embeddings, allow_dangerous_deserialization=True)
+    retriever = vector_db.as_retriever(search_kwargs={"k": 5})
+else:
+    st.error(f"FAISS index files not found at {load_path}. Ensure both index.faiss and index.pkl are present.")
+
+# Initialize memory
+@st.cache_resource
+def init_memory(_llm):
+    return ConversationBufferMemory(
+        llm=_llm,
+        output_key='answer',
+        memory_key='chat_history',
+        return_messages=True)
+memory = init_memory(llm)
+
+# Prompt template
+input_template = """Answer the question based only on the following context.
+Keep your answers short and succinct, but always use whole sentences.
+All answers in German.
+Important: Always add the link
+
+Previous conversation:
+{chat_history}
+
+Context to answer question:
+{context}
+
+Question to be answered: {question}
+Response:"""
+
+prompt = PromptTemplate(template=input_template, input_variables=["context", "question"])
+
+chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory, return_source_documents=False, combine_docs_chain_kwargs={"prompt": prompt})
+
+
+
 # Create a Flask app to handle file uploads
 app = Flask(__name__)
 
