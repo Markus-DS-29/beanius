780c8965e551c7b2f8a76ef02e4d1784cf8fca0f
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-24 10:59:31 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add the directory containing st_audiorec to the system path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'path/to/your/modified/st_audiorec'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'st_audiorec'))
 
 from st_audiorec import st_audiorec
 
