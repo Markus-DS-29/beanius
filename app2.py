2d5b14bcb30e6181638247f8732c1fa070c854b5
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-24 09:42:47 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,3 +1,10 @@
+import os
+import streamlit.components.v1 as components
+
+def st_audiorec():
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    return components.declare_component("st_audiorec", path=build_dir)
 import sys
 import streamlit as st
 from st_audiorec import st_audiorec  # Import the st_audiorec function
