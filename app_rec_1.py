6cd6cdba4001af111d01796889e2136929dad86b
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-24 11:06:51 +0200

    Create app_rec_1.py

---
+++ app_rec_1.py
@@ -0,0 +1,12 @@
+from streamlit_mic_recorder import mic_recorder
+audio = mic_recorder(
+    start_prompt="Start recording",
+    stop_prompt="Stop recording",
+    just_once=False,
+    use_container_width=False,
+    format="webm",
+    callback=None,
+    args=(),
+    kwargs={},
+    key=None
+)
