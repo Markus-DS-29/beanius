bf6c15ca9e33c6ccf9db5eb4fab9928aa3775ad3
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-24 11:16:13 +0200

    Update app_rec_1.py

--- app_rec_1.py
+++ app_rec_1.py
@@ -10,16 +10,10 @@ c1, c2 = st.columns(2)
 with c1:
     st.write("Convert speech to text:")
 with c2:
-    text = speech_to_text(language='en', use_container_width=True, just_once=True, key='STT')
+    text = speech_to_text(language='de', use_container_width=True, just_once=True, key='STT')
 
 if text:
     state.text_received.append(text)
 
 for text in state.text_received:
     st.text(text)
-
-st.write("Record your voice, and play the recorded audio:")
-audio = mic_recorder(start_prompt="⏺️", stop_prompt="⏹️", key='recorder')
-
-if audio:
-    st.audio(audio['bytes'])
