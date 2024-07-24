318051ced2fcb20c1f7ee95c7c164ae65ece4759
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-24 11:14:30 +0200

    Update app_rec_1.py

--- app_rec_1.py
+++ app_rec_1.py
@@ -1,12 +1,25 @@
-from streamlit_mic_recorder import mic_recorder
-audio = mic_recorder(
-    start_prompt="Start recording",
-    stop_prompt="Stop recording",
-    just_once=False,
-    use_container_width=False,
-    format="webm",
-    callback=None,
-    args=(),
-    kwargs={},
-    key=None
-)
+import streamlit as st
+from streamlit_mic_recorder import mic_recorder, speech_to_text
+
+state = st.session_state
+
+if 'text_received' not in state:
+    state.text_received = []
+
+c1, c2 = st.columns(2)
+with c1:
+    st.write("Convert speech to text:")
+with c2:
+    text = speech_to_text(language='en', use_container_width=True, just_once=True, key='STT')
+
+if text:
+    state.text_received.append(text)
+
+for text in state.text_received:
+    st.text(text)
+
+st.write("Record your voice, and play the recorded audio:")
+audio = mic_recorder(start_prompt="⏺️", stop_prompt="⏹️", key='recorder')
+
+if audio:
+    st.audio(audio['bytes'])
