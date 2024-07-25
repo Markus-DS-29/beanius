079ad4572c3acdfa7e9504dcfd74d3d8e30c7119
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-25 07:02:57 +0200

    Delete app3.py

--- app3.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add the directory containing st_audiorec to the system path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'st_audiorec'))
-
-from st_audiorec import st_audiorec
-
-wav_audio_data = st_audiorec()
