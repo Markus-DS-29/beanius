415146373564e747b23a2934ada02e26c52eadca
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-26 17:21:35 +0200

    Update app_rec_3.py

--- app_rec_3.py
+++ app_rec_3.py
@@ -71,7 +71,7 @@ def detect_and_replace_url(answer):
             detected_slug = None
         #answer = url_pattern.sub('[Info](/single_bean)', answer)
 #########        # Display a link that opens in the same tab
-        answer = url_pattern.sub('<a href="/single_bean?url={detected_slug}" target="_self">Hier klicken für mehr Infos.</a>', answer)
+        answer = url_pattern.sub(f'<a href="/single_bean?url={detected_slug}" target="_self">Hier klicken für mehr Infos.</a>', answer)
     else:
         detected_url = None
         detected_slug = None
