7c0d7a55bd3208499ac8c8943cb1b0345c7d73d4
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-26 19:26:09 +0200

    Update single_bean.py

--- pages/single_bean.py
+++ pages/single_bean.py
@@ -23,7 +23,6 @@ def fetch_single_beans_info_from_db(source_url):
     cursor.execute('SELECT title, source_url, rating_value, review_count, description FROM beans_info WHERE source_url = %s', (source_url,))
     beans_info = cursor.fetchone()
     
-    #cursor.close()
     conn.close()
     
     return beans_info
@@ -56,11 +55,25 @@ def display_single_beans_info(source_url):
 if __name__ == "__main__":
     query_params = st.experimental_get_query_params()
     source_url = query_params.get('url', [None])[0]
+    session_id = query_params.get('session_id', [None])[0]
+    
     if source_url:
         # Decode the URL from the query parameters
         decoded_slug = urllib.parse.unquote(source_url)
         external_url = "https://www.kaffeezentrale.de/"
         decoded_url = external_url + decoded_slug
+        
+        # Display the beans information
         display_single_beans_info(decoded_url)
     else:
         st.markdown("**Error:** No URL detected in the query parameters.")
+    
+    # Create a link to the main page with the session_id
+    if session_id:
+        main_page_url = f"/?session_id={session_id}"
+    else:
+        main_page_url = "/"
+
+    # Add a button to navigate back to the main page
+    if st.button('Back to Main Page'):
+        st.markdown(f'<a href="{main_page_url}" target="_self">Back to Main Page</a>', unsafe_allow_html=True)
