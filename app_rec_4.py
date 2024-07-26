3d8ee6ed485af038586735d115a8b81780ee6aee
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-26 19:32:14 +0200

    Update app_rec_4.py

--- app_rec_4.py
+++ app_rec_4.py
@@ -93,8 +93,9 @@ def detect_and_replace_url(answer):
         existing_params['session_id'] = st.session_state.session_id
         # Generate URL with both parameters
         subpage_url = f"/single_bean?{urlencode(existing_params)}"
-
-        answer = url_pattern.sub(f'<a href="{subpage_url}" target="_self">Hier klicken für mehr Infos.</a>', answer)
+        #answer = url_pattern.sub(f'<a href="{subpage_url}" target="_self">Hier klicken für mehr Infos.</a>', answer)
+        answer = url_pattern.sub(f'[Info]({subpage_url})', answer)
+        #answer = url_pattern.sub(f'<a href="{subpage_url}" target="_self">Hier klicken für mehr Infos.</a>', answer)
     else:
         detected_url = None
         detected_slug = None
