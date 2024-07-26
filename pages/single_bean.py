03ddacbf2b0226f95c3fd8db8066de16c0035707
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-07-26 11:46:35 +0200

    Update single_bean.py

--- pages/single_bean.py
+++ pages/single_bean.py
@@ -41,7 +41,7 @@ def display_single_beans_info(source_url):
 
         if beans_info['review_count'] > 0:
             chart_data_rating = pd.DataFrame({"Rating": [beans_info['rating_value']]})
-            st.bar_chart(chart_data_rating, y="Rating", use_container_width=True)
+            st.bar_chart(chart_data_rating, y="Rating", horizontal=True)
             st.markdown(f"**Reviews:** {beans_info['review_count']}")
         else:
             st.markdown("**Reviews:** No reviews yet.")
