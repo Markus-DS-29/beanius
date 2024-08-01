2835a83ed8272891161688a24cc324b0c50b9c05
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-08-01 13:43:17 +0200

    Update app_impro_3.py

--- app_impro_3.py
+++ app_impro_3.py
@@ -467,11 +467,14 @@ if not st.session_state.awaiting_feedback:
         
         # Store the prompt and set awaiting feedback state
         st.session_state.last_prompt = prompt
-        st.session_state.awaiting_feedback = True
-
+        #st.session_state.awaiting_feedback = True
+                
+        def on_change_to_yes():
+                st.session_state.awaiting_feedback = True
+                
         # Display feedback options
-        st.radio("Do you want to improve this answer?", ('No', 'Yes'), key='feedback_radio')
-
+        st.radio("Do you want to improve this answer?", ('No', 'Yes'), on_change=on_change_to_yes(), key='feedback_radio')
+ 
 else:
     # Show feedback form
     display_feedback_form()
