a404e1e05f61637bc2af64217b5ad58fd879aa18
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-08-01 15:36:22 +0200

    Update app_impro_3.py

--- app_impro_3.py
+++ app_impro_3.py
@@ -465,13 +465,14 @@ if prompt := st.chat_input("Was für einen Espresso suchst du?"):
                 
         def on_change_to_yes():
             st.session_state.awaiting_feedback = True
-                            
+            display_feedback_form()
+                
         # Display feedback options
         st.write(f"st.session_state.awaiting_feedback vor buttons: {st.session_state.awaiting_feedback}")
         st.radio("Do you want to improve this answer?", ('No', 'Yes'), on_change=on_change_to_yes, key='feedback_radio')
         st.write(f"st.session_state.awaiting_feedback nach buttons: {st.session_state.awaiting_feedback}")
         
-display_feedback_form()
+#display_feedback_form()
 
 #st.session_state.awaiting_feedback = False
 st.write(f"st.session_state.awaiting_feedback am Ende: {st.session_state.awaiting_feedback}")
