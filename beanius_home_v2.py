3eb5aafc9095a8a1e48d8cd18fa6143fbb7002cf
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-08-04 19:18:25 +0200

    Update beanius_home_v2.py

--- beanius_home_v2.py
+++ beanius_home_v2.py
@@ -21,8 +21,10 @@ import uuid
 from urllib.parse import urlencode
 
 # Audio
-from libs.streamlit_mic_recorder_main import mic_recorder, speech_to_text
-from streamlit_mic_recorder import speech_to_text
+from streamlit_mic_recorder import mic_recorder, speech_to_text
+
+###trying to host it in my repo doesn't work...
+#from libs.streamlit_mic_recorder_main import mic_recorder, speech_to_text
 #from libs.streamlit_mic_recorder_main.streamlit_mic_recorder.audio_recorder import audio_recorder_function
 #from libs.streamlit_mic_recorder_main.streamlit_mic_recorder.recorder import recorder_function
 
