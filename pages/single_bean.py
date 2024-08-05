f74b020091986ca93c0de317ac21888a4cf65bf3
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-08-05 17:43:25 +0200

    Update single_bean.py

--- pages/single_bean.py
+++ pages/single_bean.py
@@ -101,6 +101,7 @@ def display_single_beans_info(source_url):
         )
             
         # Show the chart in Streamlit
+        st.write("Die Eigenschaften der ausgewählten Bohne:")
         st.plotly_chart(fig)
                         
         ######## end rader ########
