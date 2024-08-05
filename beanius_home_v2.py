b8a483db7a219f3830385d69db432239f1338c22
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-08-05 10:21:12 +0200

    Update beanius_home_v2.py

--- beanius_home_v2.py
+++ beanius_home_v2.py
@@ -316,7 +316,7 @@ session_id = st.session_state.session_id
 #st.write(f"Session ID: {session_id}")
 
 ####### Initialize chat history and fetch conversations #######
-path_to_user_avatar = "images/user_icon.png"
+path_to_user_avatar = "https://github.com/Markus-DS-29/beanius/blob/main/images/user_icon.png"
 
 # Initialize chat history if not already initialized
 if "messages" not in st.session_state:
