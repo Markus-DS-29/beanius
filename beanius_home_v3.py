3fcefc591fd815d4068bb1dda7db478395f271d5
Markus-DS-29 <168541748+Markus-DS-29@users.noreply.github.com>
2024-08-06 09:24:31 +0200

    cleaning 1

--- beanius_home_v3.py
+++ beanius_home_v3.py
@@ -1,4 +1,4 @@
-####### Status: Everything seems to be working. This version is for better performance only. ###########
+####### FINAL? Status: Everything seems to be working. This version is for better performance only. ###########
 
 import os
 import pandas as pd
@@ -59,39 +59,22 @@ section[data-testid="stSidebar"]{
   height: 100px;
   margin-right: 30px;
 }
-
-
-
 </style>
 """
+
 # Inject CSS into the Streamlit app
 st.markdown(css, unsafe_allow_html=True)
 
 
-### Initialize chat history and feedback state
+# Initialize chat history and feedback state
 if 'messages' not in st.session_state:
     st.session_state.messages = []
 if 'query_data' not in st.session_state:
     st.session_state.query_data = ""
 
+### Get and save RAG-data from database ###
 
-
-# Database connection configuration for conversations
-db_config = {
-    'user': st.secrets["mysql"]["user"],
-    'password': st.secrets["mysql"]["password"],
-    'host': st.secrets["mysql"]["host"],
-    'database': st.secrets["mysql"]["database"]
-}
-
-# Function to connect to the database
-def get_db_connection():
-    return mysql.connector.connect(**db_config)
-
-
-### Start: Get content from database ###
-
-# Database connection configuration for data
+# Database connection configuration for RAG data
 db_config_data = {
     'user': st.secrets["mysql_data"]["user"],
     'password': st.secrets["mysql_data"]["password"],
@@ -99,12 +82,11 @@ db_config_data = {
     'database': st.secrets["mysql_data"]["database_2"]
 }
 
-# Function to connect to the database
+# Function to connect to the database for RAG data 
 def get_db_connection_2():
     return mysql.connector.connect(**db_config_data)
 
-
-# Function to fetch content from the database
+# Function to fetch RAG-chunks from the database
 def fetch_chunks_sql_from_db():
     conn = get_db_connection_2()
     cursor = conn.cursor(dictionary=True)
@@ -117,6 +99,7 @@ def fetch_chunks_sql_from_db():
     conn.close()
     return chunks_sql
 
+# Function to fetch RAG-feedback from the database
 def fetch_feedback_sql_from_db():
     conn = get_db_connection_2()
     cursor = conn.cursor(dictionary=True)
@@ -129,25 +112,34 @@ def fetch_feedback_sql_from_db():
     conn.close()
     return feedback_sql
 
-### End: Get RAG from database ###
+### Get and save conversations from database
+
+# Database connection configuration for conversations
+db_config = {
+    'user': st.secrets["mysql"]["user"],
+    'password': st.secrets["mysql"]["password"],
+    'host': st.secrets["mysql"]["host"],
+    'database': st.secrets["mysql"]["database"]
+}
+
+# Function to connect to the database for conversations
+def get_db_connection():
+    return mysql.connector.connect(**db_config)
 
 # Function to save conversations to the database
 def save_conversations_to_db(messages, session_id):
     conn = get_db_connection()
     cursor = conn.cursor()
-    
     for message in messages:
         cursor.execute('''
             INSERT INTO conversations (timestamp, role, content, session_id)
             VALUES (%s, %s, %s, %s)
         ''', (datetime.now(), message['role'], message['content'], session_id))
-    
     conn.commit()
     cursor.close()
     conn.close()
 
 # Function to fetch conversations from the database
-
 def fetch_conversations_from_db(session_id):
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
@@ -168,7 +160,7 @@ def detect_and_replace_url(answer):
     base_url = "https://www.kaffeezentrale.de/"
     urls = url_pattern.findall(answer)
     if urls:
-        detected_url = urls[0].rstrip('>,)')
+        detected_url = urls[0].rstrip('>,).')
         if detected_url.startswith(base_url):
             detected_slug = detected_url[len(base_url):]
         else:
@@ -181,7 +173,6 @@ def detect_and_replace_url(answer):
         subpage_url = f"/single_bean?{urlencode(existing_params)}"
         answer = url_pattern.sub(f'<a href="{subpage_url}" target="_self">Hier klicken für mehr Infos.</a>', answer)
         #answer = url_pattern.sub(f'[Info]({subpage_url})', answer)
-        
     else:
         detected_url = None
         detected_slug = None
@@ -192,10 +183,12 @@ def detect_and_replace_url(answer):
     
     return answer
 
-### Start: Combine data from db ###
+### Combine data from db ###
+
 # Fetch data from the database
 chunks_data = fetch_chunks_sql_from_db()
 feedback_data = fetch_feedback_sql_from_db()
+
 # Convert the list of dictionaries to a DataFrame
 chunks_sql_df = pd.DataFrame(chunks_data)
 feedback_sql_df = pd.DataFrame(feedback_data)
@@ -207,29 +200,6 @@ feedback_text = feedback_sql_df[['combined_text']]
 # Concatenate the DataFrames vertically
 all_data_df = pd.concat([chunks_text, feedback_text], ignore_index=True)
 
-### End: Combine data from db ###
-
-### Start: saving feedback to SQL database ###
-
-# Function to get the next available index for feedback
-def get_next_index():
-    try:
-        conn = get_db_connection_2()
-        cursor = conn.cursor()
-        query = "SELECT MAX(`index`) FROM feedback_db"
-        cursor.execute(query)
-        result = cursor.fetchone()
-        current_max_index = result[0]
-        if current_max_index is None:
-            return 1
-        else:
-            return current_max_index + 1
-    except Error as e:
-        st.error(f"Error while fetching next index: {e}")
-        return 1
-    finally:
-        cursor.close()
-        conn.close()
 
 # Connection to HuggingFace
 huggingface_token = st.secrets["api_keys"]["df_token"]
@@ -245,10 +215,9 @@ load_path = "coffee_content/faiss_index"
 os.makedirs(embeddings_folder, exist_ok=True)
 os.makedirs(load_path, exist_ok=True)
 
-### Start: FAISS
-### Start: FAISS cache
+### Start FAISS
 
-# Function to create FAISS vector store
+# Function to create and cache FAISS vector store
 @cache_resource
 def create_faiss_vector_store(dataframe, embedding_model, embeddings_folder):
     # Initialize embeddings
@@ -262,23 +231,22 @@ def create_faiss_vector_store(dataframe, embedding_model, embeddings_folder):
     vector_db = FAISS.from_documents(documents, embeddings)
     return vector_db
 
-### End FAISS cache
-
 # Create FAISS vector store using function for cache
 vector_db = create_faiss_vector_store(all_data_df, embedding_model, embeddings_folder)
 retriever = vector_db.as_retriever(search_kwargs={"k": 1})
 memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer')
 #st.write("FAISS vector store created successfully.")
 
-### End: FAISS ###
+### Embeddings, memory, prompt
 
-@st.cache_resource #create function
+# Embeddings
+@st.cache_resource   -> caching? 
 def init_embeddings():
     return HuggingFaceEmbeddings(model_name=embedding_model)
 embeddings = init_embeddings() 
 
 # Initialize memory
-@st.cache_resource
+#@st.cache_resource   -> caching?
 def init_memory(_llm):
     return ConversationBufferMemory(
         llm=_llm,
@@ -291,7 +259,7 @@ memory = init_memory(llm)
 input_template = """Answer the question based only on the following context.
 Keep your answers short and succinct, but always use whole sentences.
 All answers must always be in German!
-Most Important: Always add the according url to your answer, no "(" or "'" or ")". 
+Most Important: Always add the 1 according url to your answer, no "(" or "'" or ")". 
 
 Previous conversation:
 {chat_history}
@@ -310,7 +278,7 @@ chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=m
 st.title("Welcome to Beanius, your Espresso expert.")
 #st.markdown("Just give me a minute, I will be right with you.")
 
-####### Extract and handle session_id #######
+### Extract and handle session_id
 
 # Extract session_id from the URL if available
 query_params = st.experimental_get_query_params()
@@ -323,23 +291,21 @@ elif 'session_id' not in st.session_state:
     # Generate a new session_id if none exists
     st.session_state.session_id = str(uuid.uuid4())
 
-# Now you can safely use st.session_state.session_id
+# Now we can safely use st.session_state.session_id
 session_id = st.session_state.session_id
 
 # Display session ID for debugging
 #st.write(f"Session ID: {session_id}")
 
-####### Initialize chat history and fetch conversations #######
+### Initialize chat history and fetch conversations
 
-#load custom avatar images
+#load custom avatar images for user and beanius
 path_to_user_avatar = "images/user_icon.png"
 with open(path_to_user_avatar, "rb") as file:
     user_image = file.read()
-
 path_to_beanius_avatar = "images/beanius_icon.png"
 with open(path_to_beanius_avatar, "rb") as file:
     beanius_image = file.read()
-
 
 # Initialize chat history if not already initialized
 if "messages" not in st.session_state:
@@ -349,7 +315,6 @@ if "messages" not in st.session_state:
 if session_id_from_url:
     st.session_state.messages = fetch_conversations_from_db(session_id)
 
-# Display chat messages from history on app rerun ###############experimental
 # Display chat messages with avatars
 for message in st.session_state.messages:
     if message["role"] == "user":
@@ -360,17 +325,14 @@ for message in st.session_state.messages:
             st.markdown(message["content"], unsafe_allow_html=True)
 
 
-## Display chat messages from history on app rerun ###############experimental
-#for message in st.session_state.messages:
-#    with st.chat_message(message["role"]):
-#        st.markdown(message["content"], unsafe_allow_html=True)
-
-
+# Display chat messages from history on app rerun
 state = st.session_state
-
 if 'text_received' not in state:
     state.text_received = []
 
+### Speech to text
+
+# Display button and call function
 c1, c2 = st.columns(2)
 with c1:
     st.write("Was für einen Espresso suchst du?")
@@ -409,6 +371,8 @@ if transcription:
 
     # Save the updated conversation to the database
     save_conversations_to_db(st.session_state.messages, session_id)
+
+### Display user input field in addition to speech-to-text
 
 if prompt := st.chat_input("Was für einen Espresso suchst du?"):
         # Add user message to chat history
